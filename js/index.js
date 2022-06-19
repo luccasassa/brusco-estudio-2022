163239ff55692e45fb9fccaaa94413127df27ebf
Luca <luccaomg5@gmail.com>
2022-06-19 20:52:15 -0300

    arreglo de navbar (width, height, justify-content, display)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const d = document
 const hamburger = d.querySelector('.hamburger')
 const navMenu = d.querySelector('.nav-menu')
+const main = d.querySelector('main')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
@@ -13,3 +14,8 @@ d.querySelectorAll('.nav-link')
         navMenu.classList.remove('active')
     })
 )
+
+main.addEventListener('click', () => {
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+})
