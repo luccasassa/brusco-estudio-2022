4eb7fcfb32c9a4144f55a1ad49f5d26edb9b7268
Gera Barboni <gerardobarboni@gmail.com>
2022-07-16 19:12:05 -0300

    index: swiper acomodado. proyecto: swiper acomodad

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
-
-
 const d = document;
-const hamburger = d.querySelector(".hamburger");
-const navMenu = d.querySelector(".nav-menu");
-const main = d.querySelector("main");
+const hamburger = d.querySelector('.hamburger');
+const navMenu = d.querySelector('.nav-menu');
+const main = d.querySelector('main');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-d.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+d.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
   })
 );
 
-main.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+main.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 });
 
+var screenWidth = screen.width;
+console.log(screenWidth);
+if (screenWidth < 767) {
+  var noSwiperInResponsive = document.getElementById('noSwiperInResponsive');
+  noSwiperInResponsive.classList.remove('swiper-slide');
+}
